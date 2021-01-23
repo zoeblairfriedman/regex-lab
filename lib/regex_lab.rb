097a059e2b225dee4d645bae50da325e983757d0
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-23 13:15:48 -0500

    commit tree ecd81cdd87fee6cfc858fa523f400e47498064d9

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/\b[aeiouAEIOU]\w*/)
+    return true
+else
+    return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.grep(/(un\w*)+(ing)/)
 end
 
 def words_five_letters_long(text)
